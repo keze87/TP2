909977e4b69335f83430e5ce8be5df6dcb3fde21
Guido <guido.zaglul@gmail.com>
2016-11-05 10:08:02 -0300

    Métodos para los casos de prueba
    Agregué los métodos para los casos de prueba que tenemos que entregar
    este lunes. A la tarde los empiezo a codear.

--- test/BatallaTests.java
+++ test/BatallaTests.java
@@ -4,7 +4,57 @@ import org.junit.Assert.*;
 public class BatallaTests {
 	
 	@Test
-	public void test01SquirtleAtacaACharmander() {
+	public void test01SquirtleAtacaACharmanderConBurbujaYCanionDeAgua() {
+		
+	}
+	
+	@Test
+	public void test02SquirtleAtacaABulbasaurConBurbujaYCanionDeAgua() {
+		
+	}
+	
+	@Test
+	public void test03SquirtleAtacaALosOtrosAlgomonesConBurbujaYCanionDeAgua() {
+		
+	}
+	
+	@Test
+	public void test04BulbasaurYChanseyAtacanASquirtleConLatigoCepa() {
+		
+	}
+	
+	@Test
+	public void test05BulbasaurYChanseyAtacanACharmanderConLatigoCepa() {
+		
+	}
+	
+	@Test
+	public void test06BulbasaurYChanseyAtacanALosOtrosAlgomonesConLatigoCepa() {
+		
+	}
+	
+	@Test
+	public void test07CharmanderAtacaABulbasaurConBrasas() {
+		
+	}
+	
+	@Test
+	public void test08CharmanderAtacaASquirtleConBrasas() {
+		
+	}
+	
+	@Test
+	public void test09CharmanderAtacaALosOtrosAlgomonesConBrasas() {
+		
+	}
+	
+	@Test
+	public void test10AlgomonesAtacanConAtaqueRapido() {
+		
+	}
+	
+	@Test
+	public void test11AlgomonesAgotanCantidadDisponibleDeAtaques() {
 		
 	}
 }
