5a5ec10b112adf54ca32dabd80dc28cc0b5875b1
Luis Tejerina <lteje11@gmail.com>
2016-11-06 15:41:10 +0200

    Test tipo

---
+++ test/fiuba/algo3/modelo/TipoTests.java
@@ -0,0 +1,74 @@
+package test.fiuba.algo3.modelo;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import src.fiuba.algo3.modelo.*;
+
+public class TipoTests {
+	
+	@Test
+	public void test01MultiplicadoresTipoFuego() {
+		
+		Tipo fuego = new Fuego();
+		Tipo otroFuego = new Fuego();
+		Tipo agua = new Agua();
+		Tipo planta = new Planta();
+		Tipo normal = new Normal();
+		
+		assertEquals(0.5f, fuego.getMultiplicadorContra(otroFuego), 0.0001D);		
+		assertEquals(0.5f, fuego.getMultiplicadorContra(agua), 0.0001D);
+		assertEquals(2f, fuego.getMultiplicadorContra(planta), 0.0001D);
+		assertEquals(1f, fuego.getMultiplicadorContra(normal), 0.0001D);
+		
+	}
+	
+	@Test
+	public void test02MultiplicadoresTipoAgua() {
+		
+		Tipo fuego = new Fuego();		
+		Tipo agua = new Agua();
+		Tipo otroAgua = new Agua();
+		Tipo planta = new Planta();
+		Tipo normal = new Normal();	
+		
+		
+		assertEquals(2f, agua.getMultiplicadorContra(fuego), 0.0001D);
+		assertEquals(0.5f, agua.getMultiplicadorContra(otroAgua), 0.0001D);
+		assertEquals(0.5f, agua.getMultiplicadorContra(planta), 0.0001D);
+		assertEquals(1f, agua.getMultiplicadorContra(normal), 0.0001D);
+		
+	}
+	
+	@Test
+	public void test03MultiplicadoresTipoPlanta() {
+		
+		Tipo fuego = new Fuego();
+		Tipo agua = new Agua();
+		Tipo planta = new Planta();
+		Tipo otroPlanta = new Planta();
+		Tipo normal = new Normal();
+		
+		assertEquals(0.5f, planta.getMultiplicadorContra(fuego), 0.0001D);
+		assertEquals(2f, planta.getMultiplicadorContra(agua), 0.0001D);
+		assertEquals(0.5f, planta.getMultiplicadorContra(otroPlanta), 0.0001D);
+		assertEquals(1f, planta.getMultiplicadorContra(normal), 0.0001D);
+		
+	}
+	
+	@Test
+	public void test04MultiplicadoresTipoNormal() {
+		
+		Tipo fuego = new Fuego();	
+		Tipo agua = new Agua();
+		Tipo planta = new Planta();
+		Tipo normal = new Normal();
+		Tipo otroNormal = new Normal();
+		
+		assertEquals(1f, normal.getMultiplicadorContra(fuego), 0.0001D);
+		assertEquals(1f, normal.getMultiplicadorContra(agua), 0.0001D);
+		assertEquals(1f, normal.getMultiplicadorContra(planta), 0.0001D);
+		assertEquals(1f, normal.getMultiplicadorContra(otroNormal), 0.0001D);
+		
+	}
+	
+}
