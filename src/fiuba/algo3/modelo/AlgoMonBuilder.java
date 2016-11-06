b3b3362a0c30297fc7f04526d0dd5f44bb4757e6
keze87 <keze87@gmail.com>
2016-11-06 10:54:45 -0300

    Los otros builders
    Comenté todos los ataques, por si queremos hacer otro builder.
    
    E hice todos los métodos públicos para hacer cosas raras.

--- src/fiuba/algo3/modelo/AlgoMonBuilder.java
+++ src/fiuba/algo3/modelo/AlgoMonBuilder.java
@@ -3,40 +3,77 @@ import src.fiuba.algo3.modelo.ataques.Ataque;
 
 public class AlgoMonBuilder {
 
-	private String nombre = "";
-	private int vida = 0;
 	private Ataque ataque1;
 	private Ataque ataque2;
 	private Ataque ataque3;
+	private String nombre = "";
+	private int vida = 0;
 
+	public static AlgoMon crearBulbasaur() {
+
+		AlgoMonBuilder builder = new AlgoMonBuilder();
+
+		builder.nombre("Bulbasaur").vida(140);//.ataque1(new Chupavidas()).
+		//ataque2(new LatigoCepa()).ataque3(new AtaqueRapido());
+
+		return builder.crearTipoPlanta();
+
+	}
+	public static AlgoMon crearChansey() {
+
+		AlgoMonBuilder builder = new AlgoMonBuilder();
+
+		builder.nombre("Chansey").vida(130);//.ataque1(new Canto()).
+		//ataque2(new LatigoCepa()).ataque3(new AtaqueRapido());
+
+		return builder.crearTipoNormal();
+
+	}
 	public static AlgoMon crearCharmander() {
 
 		AlgoMonBuilder builder = new AlgoMonBuilder();
 
-		builder.nombre("Charmander").vida(170).ataque1(new Brasas()).
-		ataque2(new Fogonazo()).ataque3(new AtaqueRapido());
+		builder.nombre("Charmander").vida(170);//.ataque1(new Brasas()).
+		//ataque2(new Fogonazo()).ataque3(new AtaqueRapido());
 
 		return builder.crearTipoFuego();
 
 	}
+	public static AlgoMon crearJigglypuff() {
 
-	private AlgoMonBuilder vida(int vida) {
+		AlgoMonBuilder builder = new AlgoMonBuilder();
 
-		this.vida = vida;
+		builder.nombre("Jigglypuff").vida(130);//.ataque1(new Canto()).
+		//ataque2(new Burbuja()).ataque3(new AtaqueRapido());
 
-		return this;
-
-	}
-
-	private AlgoMonBuilder nombre(String nombre) {
-
-		this.nombre = nombre;
-
-		return this;
+		return builder.crearTipoNormal();
 
 	}
 
-	private AlgoMonBuilder ataque1(Ataque ataque) {
+	// https://github.com/pret/pokered/blob/master/text/maps/viridian_forest_entrance.asm
+	public static AlgoMon crearRattata() {
+
+		AlgoMonBuilder builder = new AlgoMonBuilder();
+
+		builder.nombre("Rattata").vida(170);//.ataque1(new Fogonazo()).
+		//ataque2(new Burbuja()).ataque3(new AtaqueRapido());
+
+		return builder.crearTipoNormal();
+
+	}
+
+	public static AlgoMon crearSquirtle() {
+
+		AlgoMonBuilder builder = new AlgoMonBuilder();
+
+		builder.nombre("Squirtle").vida(150);//.ataque1(new Burbuja()).
+		//ataque2(new CañonAgua()).ataque3(new AtaqueRapido());
+
+		return builder.crearTipoAgua();
+
+	}
+
+	public AlgoMonBuilder ataque1(Ataque ataque) {
 
 		this.ataque1 = ataque;
 
@@ -44,7 +81,7 @@ public class AlgoMonBuilder {
 
 	}
 
-	private AlgoMonBuilder ataque2(Ataque ataque) {
+	public AlgoMonBuilder ataque2(Ataque ataque) {
 
 		this.ataque2 = ataque;
 
@@ -52,7 +89,7 @@ public class AlgoMonBuilder {
 
 	}
 
-	private AlgoMonBuilder ataque3(Ataque ataque) {
+	public AlgoMonBuilder ataque3(Ataque ataque) {
 
 		this.ataque3 = ataque;
 
@@ -60,33 +97,44 @@ public class AlgoMonBuilder {
 
 	}
 
-	private AlgoMon crearTipoFuego() {
+	public AlgoMonBuilder nombre(String nombre) {
+
+		this.nombre = nombre;
+
+		return this;
+
+	}
+
+	public AlgoMonBuilder vida(int vida) {
+
+		this.vida = vida;
+
+		return this;
+
+	}
+
+	public AlgoMon crearTipoAgua() {
+
+		return new AlgoMonAgua(nombre, vida, ataque1, ataque2, ataque3);
+
+	}
+
+	public AlgoMon crearTipoFuego() {
+
 		return new AlgoMonFuego(nombre, vida, ataque1, ataque2, ataque3);
+
 	}
 
-	public static AlgoMon crearBulbasaur() {
-		// TODO Auto-generated method stub
-		return null;
+	public AlgoMon crearTipoNormal() {
+
+		return new AlgoMonNormal(nombre, vida, ataque1, ataque2, ataque3);
+
 	}
 
-	public static AlgoMon crearSquirtle() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public AlgoMon crearTipoPlanta() {
 
-	public static AlgoMon crearJigglypuff() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		return new AlgoMonPlanta(nombre, vida, ataque1, ataque2, ataque3);
 
-	public static AlgoMon crearChansey() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public static AlgoMon crearRattata() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
