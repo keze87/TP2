037aa6b29b52468931eae95bf64e30b7f183e769
Keze <keze87@gmail.com>
2016-11-06 09:53:00 -0300

    Fix #8

--- test/fiuba/algo3/modelo/AlgoMonTest.java
+++ test/fiuba/algo3/modelo/AlgoMonTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import src.fiuba.algo3.modelo.*;
 
-public class AlgoMonTests {
+public class AlgoMonTest {
 
 	@Test
 	public void test01SquirtleAtacaACharmanderConBurbujaYCanionDeAgua() {
