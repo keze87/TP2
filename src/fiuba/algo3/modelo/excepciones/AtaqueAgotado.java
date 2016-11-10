9b9301cce2e94d8069338f5b4dfaf8ad1127828f
Guido <guido.zaglul@gmail.com>
2016-11-10 09:48:22 -0300

    Paquete para las excepciones
    Moví las excepciones a un paquete nuevo, y saqué el constructor por
    defecto para que siempre reciban un mensaje que después se pueda mostrar
    en la salida del juego.

--- src/fiuba/algo3/modelo/excepciones/AtaqueAgotado.java
+++ src/fiuba/algo3/modelo/excepciones/AtaqueAgotado.java
@@ -1,12 +1,8 @@
-package src.fiuba.algo3.modelo.ataques;
+package src.fiuba.algo3.modelo.excepciones;
 
 public class AtaqueAgotado extends RuntimeException {
 
 	private static final long serialVersionUID = 5613242410037188327L;
-
-	public AtaqueAgotado() {
-		super();
-	}
 
 	public AtaqueAgotado(String mensaje) {
 		super(mensaje);
---
+++ src/fiuba/algo3/modelo/excepciones/VidaCompleta.java
@@ -0,0 +1,12 @@
+package src.fiuba.algo3.modelo.excepciones;
+
+public class VidaCompleta extends RuntimeException {
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public VidaCompleta(String mensaje) {
+		super(mensaje);
+	}
+}
