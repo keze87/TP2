992a661ce10a9da54573c7e18e1b7b0daf34e112
wjma <walterj93ma@gmail.com>
2016-11-11 11:34:39 -0300

    Mas test en ElementosTest

--- test/fiuba/algo3/modelo/ElementosTest.java
+++ test/fiuba/algo3/modelo/ElementosTest.java
@@ -54,7 +54,7 @@ public class ElementosTest {
 	}
 
 	@Test
-	public void test03VitaminaAumentaEn2LaCantidadDeTodosLosAtaques(){
+	public void test03VitaminaAumentaEn2LaCantidadDeTodosLosAtaquesDeCharmander(){
 		AlgoMon charmander = AlgoMonBuilder.crearCharmander();
 		AlgoMon squirtle = AlgoMonBuilder.crearSquirtle();
 		AlgoMon otroCharmander = AlgoMonBuilder.crearCharmander();
@@ -162,6 +162,252 @@ public class ElementosTest {
 		rattata.atacar(NombreAtaque.ATAQUERAPIDO, jigglypuff);
 		assertEquals(jigglypuff.getVidaMaxima() - 10, jigglypuff.getVida(), 0.0001);
 		assertEquals(rattata.getVidaMaxima() - 19, rattata.getVida(), 0.0001D);
+	}
+
+	@Test
+	public void test09VitaminaAumentaEn2LaCantidadDeTodosLosAtaquesDeSquirtle(){
+		AlgoMon bulbasaur = AlgoMonBuilder.crearBulbasaur();
+		AlgoMon squirtle = AlgoMonBuilder.crearSquirtle();
+		AlgoMon otroBulbasaur = AlgoMonBuilder.crearBulbasaur();
+		AlgoMon tercerBulbasaur = AlgoMonBuilder.crearBulbasaur();
+		Vitamina vitamina = new Vitamina();
+
+		squirtle.recibirElemento(vitamina);
+		int contador = 0;
+
+		try{
+			while(true){
+				squirtle.atacar(NombreAtaque.BURBUJA, bulbasaur);
+				contador++;
+			}
+
+		} catch(AtaqueAgotado e) {
+			assertEquals(17, contador);
+		}
+
+		contador = 0;
+
+		try{
+			while(true){
+				squirtle.atacar(NombreAtaque.CAÑONDEAGUA, otroBulbasaur);
+				contador++;
+			}
+
+		} catch(AtaqueAgotado e) {
+			assertEquals(10, contador);
+		}
+
+		contador = 0;
+
+		try{
+			while(true){
+				squirtle.atacar(NombreAtaque.ATAQUERAPIDO, tercerBulbasaur);
+				contador++;
+			}
+
+		} catch(AtaqueAgotado e) {
+			assertEquals(18, contador);
+		}
+
+	}
+
+	@Test
+	public void test10VitaminaAumentaEn2LaCantidadDeTodosLosAtaquesDeBulbasaur(){
+		AlgoMon bulbasaur = AlgoMonBuilder.crearBulbasaur();
+		AlgoMon squirtle = AlgoMonBuilder.crearSquirtle();
+		AlgoMon charmander = AlgoMonBuilder.crearCharmander();
+		AlgoMon otroSquirtle = AlgoMonBuilder.crearSquirtle();
+		Vitamina vitamina = new Vitamina();
+
+		bulbasaur.recibirElemento(vitamina);
+		int contador = 0;
+
+		try{
+			while(true){
+				bulbasaur.atacar(NombreAtaque.CHUPAVIDAS, squirtle);
+				contador++;
+			}
+
+		} catch(AtaqueAgotado e) {
+			assertEquals(10, contador);
+		}
+
+		contador = 0;
+
+		try{
+			while(true){
+				bulbasaur.atacar(NombreAtaque.LATIGOCEPA, charmander);
+				contador++;
+			}
+
+		} catch(AtaqueAgotado e) {
+			assertEquals(12, contador);
+		}
+
+		contador = 0;
+
+		try{
+			while(true){
+				bulbasaur.atacar(NombreAtaque.ATAQUERAPIDO, otroSquirtle);
+				contador++;
+			}
+
+		} catch(AtaqueAgotado e) {
+			assertEquals(18, contador);
+		}
+
+	}
+
+	@Test
+	public void test11VitaminaAumentaEn2LaCantidadDeTodosLosAtaquesDeJigglypuff(){
+		AlgoMon jigglypuff = AlgoMonBuilder.crearJigglypuff();
+		AlgoMon squirtle = AlgoMonBuilder.crearSquirtle();
+		AlgoMon charmander = AlgoMonBuilder.crearCharmander();
+		AlgoMon otroCharmarder = AlgoMonBuilder.crearCharmander();
+		Vitamina vitamina = new Vitamina();
+
+		jigglypuff.recibirElemento(vitamina);
+		int contador = 0;
+
+		try{
+			while(true){
+				jigglypuff.atacar(NombreAtaque.CANTO, charmander);
+				contador++;
+			}
+
+		} catch(AtaqueAgotado e) {
+			assertEquals(8, contador);
+		}
+
+		contador = 0;
+
+		try{
+			while(true){
+				jigglypuff.atacar(NombreAtaque.BURBUJA, charmander);
+				contador++;
+			}
+
+		} catch(AtaqueAgotado e) {
+			assertEquals(17, contador);
+		}
+
+		contador = 0;
+
+		while(otroCharmarder.estaVivo()){
+			jigglypuff.atacar(NombreAtaque.ATAQUERAPIDO, otroCharmarder);
+			contador++;
+		}
+
+		jigglypuff.atacar(NombreAtaque.ATAQUERAPIDO, squirtle);
+		contador++;
+
+		try{
+			jigglypuff.atacar(NombreAtaque.BURBUJA, charmander);
+
+		} catch(AtaqueAgotado e) {
+			assertEquals(18, contador);
+		}
+	}
+
+	@Test
+	public void test12VitaminaAumentaEn2LaCantidadDeTodosLosAtaquesDeChansey(){
+		AlgoMon chansey = AlgoMonBuilder.crearChansey();
+		AlgoMon charmander = AlgoMonBuilder.crearCharmander();
+		AlgoMon otroCharmander = AlgoMonBuilder.crearCharmander();
+		Vitamina vitamina = new Vitamina();
+
+		chansey.recibirElemento(vitamina);
+		int contador = 0;
+
+		try{
+			while(true){
+				chansey.atacar(NombreAtaque.CANTO, charmander);
+				contador++;
+			}
+
+		} catch(AtaqueAgotado e) {
+			assertEquals(8, contador);
+		}
+
+		contador = 0;
+
+		try{
+			while(true){
+				chansey.atacar(NombreAtaque.LATIGOCEPA, charmander);
+				contador++;
+			}
+
+		} catch(AtaqueAgotado e) {
+			assertEquals(12, contador);
+		}
+
+		contador = 0;
+
+		while(otroCharmander.estaVivo()){
+			chansey.atacar(NombreAtaque.ATAQUERAPIDO, otroCharmander);
+			contador++;
+		}
+
+		chansey.atacar(NombreAtaque.ATAQUERAPIDO, charmander);
+		contador++;
+
+		try{
+			chansey.atacar(NombreAtaque.ATAQUERAPIDO, charmander);
+
+		} catch(AtaqueAgotado e) {
+			assertEquals(18, contador);
+		}
+
+	}
+
+	@Test
+	public void test13VitaminaAumentaEn2LaCantidadDeTodosLosAtaquesDeRattata(){
+		AlgoMon rattata = AlgoMonBuilder.crearRattata();
+		AlgoMon charmander = AlgoMonBuilder.crearCharmander();
+		AlgoMon squirtle = AlgoMonBuilder.crearSquirtle();
+		Vitamina vitamina = new Vitamina();
+
+		rattata.recibirElemento(vitamina);
+		int contador = 0;
+
+		try{
+			while(true){
+				rattata.atacar(NombreAtaque.FOGONAZO, squirtle);
+				contador++;
+			}
+
+		} catch(AtaqueAgotado e) {
+			assertEquals(6, contador);
+		}
+
+		contador = 0;
+
+		try{
+			while(true){
+				rattata.atacar(NombreAtaque.BURBUJA, squirtle);
+				contador++;
+			}
+
+		} catch(AtaqueAgotado e) {
+			assertEquals(17, contador);
+		}
+
+		contador = 0;
+
+		while(charmander.estaVivo()){
+			rattata.atacar(NombreAtaque.ATAQUERAPIDO, charmander);
+			contador++;
+		}
+
+		rattata.atacar(NombreAtaque.ATAQUERAPIDO, squirtle);
+		contador++;
+
+		try{
+			rattata.atacar(NombreAtaque.ATAQUERAPIDO, squirtle);
+
+		} catch(AtaqueAgotado e) {
+			assertEquals(18, contador);
+		}
 	}
 
 }
