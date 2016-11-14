3c6066f45ca5986f8e4ec7041b5895d1f4edf433
Guido <guido.zaglul@gmail.com>
2016-11-14 09:45:39 -0300

    Nada importante

--- src/fiuba/algo3/vista/ElegirEquipo.java
+++ src/fiuba/algo3/vista/ElegirEquipo.java
@@ -27,28 +27,27 @@ public abstract class ElegirEquipo extends EscenaJuegoAlgoMon {
 		HBox filaEquipo = new HBox(20);
 		DisplayEquipo display = new DisplayEquipo(this.jugador);
 
-		filaSuperior.getChildren().add(new BotonElegirAlgoMon("Charmander", this.jugador, display));
-		filaSuperior.getChildren().add(new BotonElegirAlgoMon("Squirtle", this.jugador, display));
-		filaSuperior.getChildren().add(new BotonElegirAlgoMon("Bulbasaur", this.jugador, display));
-		filaInferior.getChildren().add(new BotonElegirAlgoMon("Jigglypuff", this.jugador, display));
-		filaInferior.getChildren().add(new BotonElegirAlgoMon("Chansey", this.jugador, display));
-		filaInferior.getChildren().add(new BotonElegirAlgoMon("Rattata", this.jugador, display));
-
 		this.finalizarSeleccion = new Button("Listo");
 		this.finalizarSeleccion.getStyleClass().add("boton-listo");
 		this.setAccionFinalizarSeleccion();
 
+		filaSuperior.getChildren().add(new BotonElegirAlgoMon("Charmander", this.jugador, display));
+		filaSuperior.getChildren().add(new BotonElegirAlgoMon("Squirtle", this.jugador, display));
+		filaSuperior.getChildren().add(new BotonElegirAlgoMon("Bulbasaur", this.jugador, display));
 		filaSuperior.setAlignment(Pos.CENTER);
+
+		filaInferior.getChildren().add(new BotonElegirAlgoMon("Jigglypuff", this.jugador, display));
+		filaInferior.getChildren().add(new BotonElegirAlgoMon("Chansey", this.jugador, display));
+		filaInferior.getChildren().add(new BotonElegirAlgoMon("Rattata", this.jugador, display));
 		filaInferior.setAlignment(Pos.CENTER);
-		filaEquipo.setAlignment(Pos.CENTER);
 
 		filaEquipo.getChildren().add(display);
 		filaEquipo.getChildren().add(this.finalizarSeleccion);
+		filaEquipo.setAlignment(Pos.CENTER);
 
 		botonera.getChildren().add(filaSuperior);
 		botonera.getChildren().add(filaInferior);
 		botonera.getChildren().add(filaEquipo);
-		//botonera.getChildren().add(this.finalizarSeleccion);
 		botonera.setAlignment(Pos.CENTER);
 
 		layout.setCenter(botonera);
