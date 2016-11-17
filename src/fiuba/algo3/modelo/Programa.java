d388dea46109c213b734810e31b8466e8beb4d1e
Guido <guido.zaglul@gmail.com>
2016-11-17 01:15:37 -0300

    Cambio escena inicial
    Cambié la escena inicial del juego para poder probar más rápido la
    escena de la batalla. Cuando terminemos deshago los cambios.

--- src/fiuba/algo3/modelo/Programa.java
+++ src/fiuba/algo3/modelo/Programa.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCombination;
 import javafx.stage.Stage;
-import src.fiuba.algo3.vista.Introduccion;
+import src.fiuba.algo3.vista.ElegirEquipoJugador1;
 
 public class Programa extends Application {
 
@@ -12,10 +12,12 @@ public class Programa extends Application {
 	public void start(Stage primaryStage) {
 		primaryStage.setFullScreen(false);
 		//Scene menuPrincipal = new MenuPrincipal(primaryStage, new Juego());
-		Scene introduccion = new Introduccion(primaryStage, new Juego());
+		//Scene introduccion = new Introduccion(primaryStage, new Juego());
+		Scene elegirAlgoMonJugador1 = new ElegirEquipoJugador1(primaryStage, new Juego());
 
 		primaryStage.setTitle("AlgoMon");
-		primaryStage.setScene(introduccion);
+		//primaryStage.setScene(introduccion);
+		primaryStage.setScene(elegirAlgoMonJugador1);
 		primaryStage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
 		primaryStage.show();
 	}
