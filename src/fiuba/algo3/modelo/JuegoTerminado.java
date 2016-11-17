a1ac8bbefb58633cd054b3e161fbdde5941bce86
Guido <guido.zaglul@gmail.com>
2016-11-17 01:25:07 -0300

    Excepciones nuevas

---
+++ src/fiuba/algo3/modelo/JuegoTerminado.java
@@ -0,0 +1,14 @@
+package src.fiuba.algo3.modelo;
+
+public class JuegoTerminado extends RuntimeException {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public JuegoTerminado(String mensaje) {
+		super(mensaje);
+	}
+
+}
---
+++ test/fiuba/algo3/modelo/AlgoMonMuerto.java
@@ -0,0 +1,13 @@
+package test.fiuba.algo3.modelo;
+
+public class AlgoMonMuerto extends RuntimeException {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public AlgoMonMuerto(String mensaje) {
+		super(mensaje);
+	}
+}
